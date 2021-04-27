016e75f2d79933570824be1dc2d231bcee0072a8
Remixrty <73853140+Remixrty@users.noreply.github.com>
2021-04-27 16:38:23 +0300

    Add files via upload

---
+++ image edit 2/image edit 2/Form4.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace image_edit_2
+{
+	partial class Form4
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(449, 12);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(390, 390);
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(128, 610);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(110, 26);
+			this.button4.TabIndex = 11;
+			this.button4.Text = "Сохранить";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 610);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(110, 26);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Открыть";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(390, 390);
+			this.pictureBox1.TabIndex = 12;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(714, 601);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(125, 40);
+			this.button8.TabIndex = 18;
+			this.button8.Text = "Критерий Брэдли-Рота";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(714, 553);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(125, 42);
+			this.button7.TabIndex = 17;
+			this.button7.Text = "Критерий Кристиана Вульфа";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(714, 517);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(125, 30);
+			this.button6.TabIndex = 16;
+			this.button6.Text = "Критерий Сауволы";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(714, 481);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(125, 30);
+			this.button5.TabIndex = 15;
+			this.button5.Text = "Критерий Ниблека";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(714, 445);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(125, 30);
+			this.button1.TabIndex = 14;
+			this.button1.Text = "Критерий Отсу";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(714, 409);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(125, 30);
+			this.button3.TabIndex = 13;
+			this.button3.Text = "Критерий Гаврилова";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(12, 569);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(110, 26);
+			this.button9.TabIndex = 19;
+			this.button9.Text = "Открыть";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// Form4
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(851, 648);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox2);
+			this.Name = "Form4";
+			this.Text = "Form4";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button9;
+	}
+}
---
+++ image edit 2/image edit 2/Form4.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace image_edit_2
+{
+	public partial class Form4 : Form
+	{
+		private static Bitmap img = null;
+		private Bitmap image = null;
+		List<double> rgbValues = new List<double>();
+		List<byte> rgbValuesb = new List<byte>();
+		public Form4()
+		{
+			InitializeComponent();
+			
+		}
+
+		//кнопки
+		#region buttons
+		private void button3_Click(object sender, EventArgs e) //критерий Гаврилова
+		{
+			double sum = 0;
+			double res = 0;
+			
+			for (int i = 0; i< rgbValues.Count; i++)
+			{
+				sum += rgbValues[i];
+			}
+			res = sum / rgbValues.Count;
+
+			for (int i = 0; i < rgbValues.Count; i+=3)
+			{
+				if ((rgbValues[i] + rgbValues[i+1] + rgbValues[i+2])/3 <= res)
+				{
+					rgbValuesb.Add(0);					
+				}
+				else
+				{
+					rgbValuesb.Add(1);
+				}
+			}
+			outbytes();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			string filename_2 = ofd.FileName;
+
+
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				
+				image = new Bitmap(ofd.FileName);
+				pictureBox1.Image = image;
+				img = new Bitmap(image.Width, image.Height);
+				pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+				inbytes();
+			}
+			
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			using (SaveFileDialog sfd = new SaveFileDialog())
+			{
+				sfd.Filter = "Файлы JPG и PNG (*.png;*.jpg;*.bmp;*.gif) | *.png;*.jpg;*.bmp;*.gif";
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					if (img != null)
+					{
+						img.Save(sfd.FileName);
+					}
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e) //критерий Отсу
+		{
+			math(image);
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		#endregion
+
+
+		//кривая работа с байтами
+		#region inout
+		public void inbytes()
+		{
+			rgbValues.Clear();
+			rgbValuesb.Clear();
+			for (int x = 0; x < image.Height; x++)
+				for (int y = 0; y < image.Width; y++)
+				{
+					var pix = image.GetPixel(y, x);
+					rgbValues.Add(0.2125*pix.R);
+					rgbValues.Add(0.7154*pix.G);
+					rgbValues.Add(0.0721*pix.B);
+				}
+		}
+
+		void outbytes()
+		{
+			int l;
+			int i = -1;
+			
+
+			for (int x = 0; x < image.Height; x++)
+				for (int y = 0; y < image.Width; y++)
+				{
+					++i;
+					if (rgbValuesb[i] == 0)
+						l = 0;
+					else
+						l = 255;
+					var pix = image.GetPixel(y, x);
+					pix = Color.FromArgb(l, l, l);
+					img.SetPixel(y, x, pix);
+				}
+			pictureBox2.Image = img;
+			pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+		}
+		#endregion
+
+		
+
+
+		//работа с байтами от Гаврилова
+		#region bytesGav
+		/*public static Bitmap math(Bitmap image)
+		{
+			int width = image.Width;
+			int height = image.Height;
+			using (Bitmap _tmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+			{
+				_tmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+				using var g = Graphics.FromImage(_tmp);
+					g.DrawImageUnscaled(image, 0, 0);
+
+
+					byte[] input_bytes = getImgBytes(_tmp);
+					byte[] out_bytes = new byte[input_bytes.Length];
+					for (int i = 0; i < input_bytes.Length; i += 3)
+					{
+						input_bytes[i] = clmp(0.2125 * input_bytes[i + 2] + 0.7154 * input_bytes[i + 1] + 0.0721 * input_bytes[0]);
+						input_bytes[i + 2] = input_bytes[i + 1] = input_bytes[i];
+					}
+
+					var t = input_bytes.Average(x => x);
+
+					var bytes = input_bytes.Select(x => (x < t) ? (byte)0 : (byte)255).ToArray();
+					int a = 10;
+					var ty = a << 2;
+
+					Bitmap img_ret = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+					img_ret.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+					writeImageBytes(img_ret, bytes);
+
+					return img_ret; 
+			}
+		
+		}
+
+		static byte clmp(double d)
+		{
+			if (d > 255)
+				return 255;
+			if (d < 0)
+				return 0;
+			return (byte)d;
+		}
+
+
+		static byte[] getImgBytes(Bitmap img)
+		{
+			byte[] bytes = new byte[img.Width * img.Height * 3];  //выделяем память под массив байтов
+
+			var data = img.LockBits(new Rectangle(0, 0, img.Width, img.Height),  //блокируем участок памати, занимаемый изображением
+				ImageLockMode.ReadOnly,
+				img.PixelFormat);
+			Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);  //копируем байты изображения в массив
+			img.UnlockBits(data);   //разблокируем изображение
+			return bytes; //возвращаем байты
+		}
+		static void writeImageBytes(Bitmap img, byte[] bytes)
+		{
+			var data = img.LockBits(new Rectangle(0, 0, img.Width, img.Height),  //блокируем участок памати, занимаемый изображением
+				ImageLockMode.WriteOnly,
+				img.PixelFormat);
+			Marshal.Copy(bytes, 0, data.Scan0, bytes.Length); //копируем байты массива в изображение
+
+			img.UnlockBits(data);  //разблокируем изображение
+		}*/
+		#endregion
+	}
+}
